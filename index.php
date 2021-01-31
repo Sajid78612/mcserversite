373ace16f5ca3a1af8b0e3a63bce73631a659219
Sajid <sajidalam786@gmail.com>
2021-01-31 19:32:21 +0000

    update

--- index.php
+++ index.php
@@ -1,138 +1,117 @@
+<?php
+require __DIR__ . '/MinecraftPing.php';
+require __DIR__ . '/MinecraftPingException.php';
+require __DIR__ . '/MinecraftSkin.php';
+
+///////////// CONFIG /////////////
+$SERVER_URL = null; // Don't set it if you dont have a server url
+$SERVER_IP 	= '77.103.3.96';
+$PORT 			= '25565';
+
+$players = array('test', 'test1'); // players to be displayed
+//////////////////////////////////
+
+$mc = new MinecraftSkin();
+
+foreach ($players as $player)
+{
+	$mc->getFace($player);
+}
+try
+{
+		$Query = new MinecraftPing( $SERVER_IP, $PORT );
+
+		$data = $Query->Query();
+		$status = 'online';
+}
+catch( MinecraftPingException $e )
+{
+		echo $e->getMessage();
+		$status = 'offline';
+}
+finally
+{
+		$Query->Close();
+}
+
+?>
+<!DOCTYPE html>
 <html>
-<head>
-	<?php include './res/templates/head.php';?>
-</head>
-<body>
-<div class="container">
-	<div class="row">
-		<ul class="collapsible popout" data-collapsible="accordion">
-			
-			<?php
-			
-				foreach ($configs->servernames as $key => $servername) {
-					
-					$serverip =  $configs->serverips[$key];
-					$serverport =  $configs->serverports[$key];
-					
-					//Server Info Getter
-						$json = file_get_contents('https://mcapi.us/server/status?ip=' . $serverip . '&port=' . $serverport);
-						$data = json_decode($json,true);
-					
-						$status = $data['status']; //Success or failure getting info (Ex. success)
-						$online = $data['online']; //Server Online or not (Ex. true)
-						$motd = $data['motd']; //Server MOTD (Ex. A Minecraft Server, Some strange formatting may be in
-											   //this, there are various libraries available for formatting this.)
-						$error = $data['error']; //Error message from the request (Error Messages: If you forget the IP
-												 //the error will be missing data. if it is an invalid IP, the error will
-												 //be invalid hostname or port. if an error starts with internal server
-												 //error, it means something is wrong with my server. empty means no error.)
-						$maxplayers = $data['players']['max']; //Maximum player limit (Ex. 20)
-						$currentplayers = $data['players']['now']; //Current number of players (Ex. 2)
-						$serverversion = $data['server']['name']; //Current server version name (Ex. Spigot 1.11.2)
-						$serverprotocol = $data['server']['protocol']; //Server version protocol (Ex. 47)
-						$lastonlinestamp = $data['last_online']; //The date the server was last recorded online. If empty, it has
-															//never been online. it is a unix timestamp in string form. if
-															//this has the same value as last_updated, it means that it is
-															//currently online. (Ex. 1431985691)
-						$lastupdatedstamp = $data['last_updated']; //The date the status of the server was last updated at. It
-															  //updates every five minutes, so you may send requests as
-															  //soon as it has expired. (Ex. 1431985691)
-															  
-					//Convert Unix Timestamps To Readable Datetimes
-						$lastonline = date('d-m-Y H:i:s', intval($lastonlinestamp));
-						$lastupdated = date('d-m-Y H:i:s', intval($lastupdatedstamp));
-						
-					echo '
-					
-					<li>
-						<div class="collapsible-header"><i class="material-icons">';
-						
-							//Online or offline response OLD (Ternary): (($online == 1)?'check_circle':"cancel")
-								
-								if ($online == 1) {
-									
-									echo "check_circle";
-									
-								}
-								else {
-									
-									echo "cancel";
-									
-								}
-							
-							echo '</i>'. $servername .'<span class="secondary-content">';
-							
-								if ($online == 1) {
-									
-									echo $currentplayers . '/' . $maxplayers;
-									
-								}
-								else {
-									
-									echo "Offline";
-									
-								}
-							
-							echo '</span></div>
-						<div class="collapsible-body"><p>';
-						
-							if ($online == 1) {
-								
-								echo '
-								
-									Online
-									<br>
-									Motd: '. $motd .'
-									<br>
-									Players: '. $currentplayers .'/'. $maxplayers .'
-									<br>
-									Server Version: '. $serverversion .'';
-									
-										if (!$lastonline == NULL) {
-											
-											echo '<br>Last Online: '. $lastonline .'';
-											
-										}
-										
-										if (!$lastupdated == NULL) {
-											
-											echo '<br>Last Updated: '. $lastupdated .'';
-											
-										}
-									
-									'
-									
-								
-								';
-								
-							}
-							else {
-								
-								if ($error == 'missing data' | $error == 'invalid hostname or port' | substr($error, 0, strlen($error)) === 'internal server error') {
-									
-									echo $error;
-									
-								}
-								else {
-									
-									echo "Server is offline.";
-									
-								}
-								
-							}
-						
-						echo '</p></div>
-					</li>
-					
-					';
-				} //foreach
-			?>
-			
-		</ul>
+	<head>
+        <meta charset="utf-8">
+        <title>Minecraft Server Status</title>
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css">
+			<link rel="shortcut icon" href="https://mgp25.com/mc/favicon.ico">
+			<link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+    	<link href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    	<script type="text/javascript" src="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    	<script>
+    	jQuery(function ($) {
+        	$("a").tooltip()
+    	});
+    	</script>
+    	<style>
+    	/*Custom CSS Overrides*/
+    	body {
+      		font-family: 'Lato', sans-serif !important;
+    	}
+    	</style>
+    </head>
+    <body>
+	<div class="container">
+        <h1>Minecraft Server Status</h1><hr>
+		<div class="row">
+			<div class="span4">
+				<h3>General Information</h3>
+				<table class="table table-striped">
+					<tbody>
+					<tr>
+					<td><b>IP</b></td>
+					<td><?php if (isset($SERVER_URL)) echo $SERVER_URL; else echo $SERVER_IP; echo ':' . $PORT; ?></td>
+					</tr>
+					<tr>
+					<td><b>Version</b></td>
+					<td><?php echo $data['version']['name']; ?></td>
+					</tr>
+					<tr>
+					<td><b>Players</b></td>
+					<td><?php echo "".$data['players']['online']." / ".$data['players']['max']."";?></td>
+					</tr>
+					<tr>
+					<td><b>Status</b></td>
+					<td><? if($status == 'online') { echo "<i class=\"icon-ok-sign\"></i> Server is online"; } else { echo "<i class=\"icon-remove-sign\"></i> Server is offline";}?></td>
+					</tr>
+					<tr>
+					<td><b>Description</b></td>
+					<td><?php echo "".$data['description']; ?></td>
+					</tr>
+					<td><b>Server image</b></td>
+					<td><?php echo '<img width="64" height="64" src="' . Str_Replace( "\n", "", $data[ 'favicon' ] ) . '">'; ?></td>
+					</tr>
+					<td><b>Mods</b></td>
+					<td><?php
+					if(isset($data['modinfo']['modList'])) { foreach ($data['modinfo']['modList'] as $mod) {$mods[] = $mod['modid'] . ' ' . $mod['version'];} echo implode(', ', array_unique($mods)); } ?></td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="span8">
+				<h3>Players</h3>
+				<?php
+				foreach($players as $key) {
+					$users .= "<a data-placement=\"top\" rel=\"tooltip\" style=\"display: inline-block;\" title=\"".$key."\">
+					<img src=\"faces/" .$key."_face.png\" size=\"40\" width=\"40\" height=\"40\" style=\"width: 40px; height: 40px; margin-bottom: 5px; margin-left: 5px; border-radius: 3px;\"/></a>";
+				}
+				if($data['players'] > 0) {
+					print_r($users);
+					}
+				else {
+					echo "<div class=\"alert\"> There are no players online at the moment!</div>";
+					}
+				?>
+			</div>
+		</div>
 	</div>
-</div>
-	
-	<!-- Load Footer -->
-	<?php include './res/templates/footer.php';?>
-</body>
+	</body>
 </html>
